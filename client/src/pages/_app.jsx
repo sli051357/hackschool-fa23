35239c01767172952708a58548078de6ecb13935
sli051357 <sli051357@gmail.com>
2023-11-01 17:20:05 -0700

    hackschool 3

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Navbar from '../components/navbar-component/navbar.jsx'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+
+  )
 }
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,18 +1,24 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css'
-
+// import acmlogo from 'client\src\assets\acmlogo.png'
+import logo from '../assets/acmlogo.png'
+//import Navbar from '../components/navbar-component/navbar.jsx'
 
 export default function Home() {
   return (
     /* Each function in JSX can only return ONE DOM element, so we have one top-level div */
-    <div className={styles.main}>
-        <div className={styles.welcome}>
-          <h1> Hello World! </h1>
-          {/* Replace the heading, and add a short paragraph about your website here! */}
-        </div>
+    <div className = {styles.main}>
+
+      <div className = {styles.welcome}>
+        <h2> Welcome to HackRacer! </h2>
+        {/* Replace the heading, and add a short paragraph about your website here! */}
+        <p> This is a typing game that I have definitely not been forced to make </p>
+      </div>
+
       <div className={styles.image}>
         {/* Put the ACM logo here! */}
         {/* Keep in mind that any images you want to use must be imported from the /assets folder */}
+        <Image src = {logo} width = {200} height = {200} alt = 'acmlogo'></Image>
       </div>
     </div>
   )
